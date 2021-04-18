e3af678ff8c6598a43ff10647811f97af937bbdd
Megha <mjain@netgear.com>
2021-04-18 17:45:13 +0530

    test

--- SwiftLintSample/ViewController.swift
+++ SwiftLintSample/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    func calculateSum(numberOne: Int, numberTwo: Int) -> Int{
+    func calculateSum(numberOne: Int, numberTwo: Int) -> Int {
         return numberOne+numberTwo
     }
 }
